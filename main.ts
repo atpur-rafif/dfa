4864c9ccb0046a91653e240ff793c6912b5c53ba
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-10-03 01:54:26 +0700

    Changed return type

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ import { SetPair } from "./src/SetPair.ts";
 function createDistingusishableTable<
     C extends readonly string[], 
     S extends readonly string[]
->(L: DFA<C, S>){
+>(L: DFA<C, S>): string{
     const table: SetPair<S[number]> = new SetPair();
 
     const charCount = L.character.length;
@@ -46,22 +46,25 @@ function createDistingusishableTable<
         if(!b) break;
     }
 
+    let res = "";
     for(let i = 1; i < stateCount; ++i){
-        let s = L.getStateByIndex(i) + " ";
+        res += L.getStateByIndex(i) + " ";
         for(let j = 0; j < Math.min(stateCount - 1, i); ++j){
             const p = L.getStateByIndex(i);
             const q = L.getStateByIndex(j);
-            if(table.has(p, q)) s += "x ";
-            else s += "  ";
+            if(table.has(p, q)) res += "x ";
+            else res += "  ";
         }
-        console.log(s);
+        res += "\n";
     }
 
-    let s = "  "
+    res += "  ";
     for(let i = 0; i < stateCount - 1; ++i){
-        s += L.getStateByIndex(i) + " ";
+        res += L.getStateByIndex(i) + " ";
     }
-    console.log(s);
+
+    return res;
 }
 
-createDistingusishableTable(M);
+const res = createDistingusishableTable(M);
+console.log(res);
