2507c79bb142f11f06d4297b043bd0c30ef8eba3
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-10-03 02:27:08 +0700

    Cleanup

--- main.ts
+++ main.ts
@@ -1,70 +1,33 @@
 import { DFA } from "./src/DFA.ts";
-import { L, M } from "./src/Declaration.ts";
-import { SetPair } from "./src/SetPair.ts";
+import { createDistingusishableTable } from "./src/Distinguishable.ts";
 
-function createDistingusishableTable<
-    C extends readonly string[], 
-    S extends readonly string[]
->(L: DFA<C, S>): string{
-    const table: SetPair<S[number]> = new SetPair();
+;export const L = new DFA(
+    ["0", "1"] as const,
+    ["A", "B", "C", "D", "E", "F", "G", "H"] as const,
+    {
+        "0": ["B", "G", "A", "C", "H", "C", "G", "G"],
+        "1": ["F", "C", "C", "G", "F", "G", "E", "C"]
+    },
+    "A",
+    ["C"]
+);
 
-    const charCount = L.character.length;
-    const stateCount = L.state.length;
-    const finalState = L.finalStateSet;
+export const M = new DFA(
+    ["0", "1"] as const,
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I"] as const,
+    {
+        "0": ["B", "C", "D", "E", "F", "G", "H", "I", "A"],
+        "1": ["E", "F", "H", "H", "I", "B", "B", "C", "E"]
+    },
+    "A",
+    ["C", "F", "I"]
+);
 
-    // Base case
-    for(let i = 0; i < stateCount - 1; ++i){
-        for(let j = 1; j < stateCount; ++j){
-            const p = L.getStateByIndex(i);
-            const q = L.getStateByIndex(j);
-            if(
-                finalState.has(p) != finalState.has(q)
-            ) table.add(p, q);
-        }
-    }
+const resL = createDistingusishableTable(L);
+console.log(resL);
 
-    // Recursive case
-    while(true){
-        let b = false;
-        for(let i = 0; i < stateCount - 1; ++i){
-            for(let j = 1; j < stateCount; ++j){
-                const p = L.getStateByIndex(i);
-                const q = L.getStateByIndex(j);
-                if(table.has(p, q)) continue;
+console.log("\n");
 
-                for(let k = 0; k < charCount; ++k){
-                    const c = L.character[k];
-                    const r = L.next(p, c);
-                    const s = L.next(q, c);
-                    if(table.has(r, s) && !b){
-                        table.add(p, q);
-                        b = true;
-                    };
-                }
-            }
-        }
-        if(!b) break;
-    }
+const resM = createDistingusishableTable(M);
+console.log(resM);
 
-    let res = "";
-    for(let i = 1; i < stateCount; ++i){
-        res += L.getStateByIndex(i) + " ";
-        for(let j = 0; j < Math.min(stateCount - 1, i); ++j){
-            const p = L.getStateByIndex(i);
-            const q = L.getStateByIndex(j);
-            if(table.has(p, q)) res += "x ";
-            else res += "  ";
-        }
-        res += "\n";
-    }
-
-    res += "  ";
-    for(let i = 0; i < stateCount - 1; ++i){
-        res += L.getStateByIndex(i) + " ";
-    }
-
-    return res;
-}
-
-const res = createDistingusishableTable(M);
-console.log(res);
---
+++ src/Distinguishable.ts
@@ -0,0 +1,65 @@
+import { DFA } from "./DFA.ts";
+import { SetPair } from "./SetPair.ts";
+
+
+export function createDistingusishableTable<
+    C extends readonly string[],
+    S extends readonly string[]
+>(L: DFA<C, S>): string {
+    const table: SetPair<S[number]> = new SetPair();
+
+    const charCount = L.character.length;
+    const stateCount = L.state.length;
+    const finalState = L.finalStateSet;
+
+    // Base case
+    for (let i = 0; i < stateCount - 1; ++i) {
+        for (let j = 1; j < stateCount; ++j) {
+            const p = L.getStateByIndex(i);
+            const q = L.getStateByIndex(j);
+            if (finalState.has(p) != finalState.has(q)) table.add(p, q);
+        }
+    }
+
+    // Recursive case
+    while (true) {
+        let b = false;
+        for (let i = 0; i < stateCount - 1; ++i) {
+            for (let j = 1; j < stateCount; ++j) {
+                const p = L.getStateByIndex(i);
+                const q = L.getStateByIndex(j);
+                if (table.has(p, q)) continue;
+
+                for (let k = 0; k < charCount; ++k) {
+                    const c = L.character[k];
+                    const r = L.next(p, c);
+                    const s = L.next(q, c);
+                    if (table.has(r, s) && !b) {
+                        table.add(p, q);
+                        b = true;
+                    };
+                }
+            }
+        }
+        if (!b) break;
+    }
+
+    let res = "";
+    for (let i = 1; i < stateCount; ++i) {
+        res += L.getStateByIndex(i) + " ";
+        for (let j = 0; j < Math.min(stateCount - 1, i); ++j) {
+            const p = L.getStateByIndex(i);
+            const q = L.getStateByIndex(j);
+            if (table.has(p, q)) res += "x ";
+            else res += "  ";
+        }
+        res += "\n";
+    }
+
+    res += "  ";
+    for (let i = 0; i < stateCount - 1; ++i) {
+        res += L.getStateByIndex(i) + " ";
+    }
+
+    return res;
+}
